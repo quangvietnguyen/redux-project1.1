772f315c948f9664e497ac03eff4f7165fab28c5
nguye374 <nguye374@sheridancollege.ca>
2021-09-29 13:34:25 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
+import store from './store/index'
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
